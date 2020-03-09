c24881600c074b6e2901389794cb39412527158c
zitxiem1998 <zitxiem1998@gmail.com>
2020-03-09 16:34:09 +0700

    abc

--- Module1/Xử lý chuỗi/Phần thực hành/Kiểm tra ký tự đầu tiên của chuỗi có phải là chữ hoa không/isUpperCase.js
+++
@@ -1,9 +0,0 @@
-function isFirstLetterUpperCase(str) {
-    var regexp = /^[A-Z]/;
-    if (regexp.test(str)) {
-        alert("chuỗi có kí tự đầu là chữ hoa");
-    } else {
-       alert("chuỗi không có kí tự đầu là chữ hoa");
-    }
-
-}
